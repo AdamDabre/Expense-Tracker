e6cd6d62f33eb9a35d5a45f0f9d39f5cc62b86ba
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-05-18 15:33:26 +0800

    [Add] apiPaths, axiosInstance, integrate login API

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,81 +1,93 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import React from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import Input from "../../components/Inputs/Input";
 import { validateEmail } from "../../utils/helper";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    // Handle login form submit
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  // Handle login form submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        if (!validateEmail(email)) {
-            setError("Please enter a valid email address.");
-            return;
-        }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
 
-        if (!password) {
-            setError("Please enter the password");
-            return;
-        }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
 
-        setError("");
+    setError("");
 
-        //Login API Call
-    };
+    // Login API Call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
 
-    return (
-        <AuthLayout>
-            <div className="lg:w-3/4 h-3/4 md:h-full flex flex-col justify-center">
-                <h3 className="text-xl font-semibold text-black">
-                    Welcome Back
-                </h3>
-                <p className="text-xs text-slate-700 mt-1 mb-6">
-                    Please enter your details to log in
-                </p>
-                <form onSubmit={handleLogin}>
-                    <Input
-                        value={email}
-                        onChange={({ target }) => setEmail(target.value)}
-                        label="Email"
-                        placeholder="john@example.com"
-                        type="email"
-                    />
-                    <Input
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                        label="Password"
-                        placeholder="Min 8 Characters"
-                        type="text"
-                    />
+      const { token, user } = response.data;
 
-                    {error && (
-                        <p className="text-red-500 text-xs pb-2.5">{error}</p>
-                    )}
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("SOmething went wrong try again.");
+      }
+    }
+  };
 
-                    <button type="submit" className="btn-primary">
-                        Login
-                    </button>
+  return (
+    <AuthLayout>
+      <div className="lg:w-3/4 h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-1 mb-6">
+          Please enter your details to log in
+        </p>
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email"
+            placeholder="john@example.com"
+            type="email"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="text"
+          />
 
-                    <p className="text-sm text-slate-800 mt-3">
-                        Dont have an account?{" "}
-                        <Link
-                            className="font-medium text-primary underline"
-                            to="/signup"
-                        >
-                            SignUp
-                        </Link>
-                    </p>
-                </form>
-            </div>
-        </AuthLayout>
-    );
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+
+          <p className="text-sm text-slate-800 mt-3">
+            Dont have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Login;
