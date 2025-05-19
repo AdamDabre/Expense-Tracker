4a407370b95c21eef69764eb429285a4b69fe204
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-05-19 14:09:32 +0800

    [Add] NavBar, SideMenu, DashBoardLayout, CharAvatar, useUserAuth, data, helper functions

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
+import SideMenu from "./SideMenu";
 
-export const Navbar = () => {
-  return <div>Navbar</div>;
+export const Navbar = ({ activeMenu }) => {
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  return (
+    <div className="flex gap-5 bg-white border border-b border-gray-200/5 backdrop-blur-sm py-4 px-7 sticky top-0 z-30">
+      <button
+        className="block lg:hidden text-black"
+        onClick={() => {
+          setOpenSideMenu(!openSideMenu);
+        }}
+      >
+        {openSideMenu ? (
+          <HiOutlineX className="text-2xl"></HiOutlineX>
+        ) : (
+          <HiOutlineMenu className="text-2xl"></HiOutlineMenu>
+        )}
+      </button>
+      <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+
+      {openSideMenu && (
+        <div className="fixed top-16 -ml-4 bg-white">
+          <SideMenu activeMenu={activeMenu} />
+        </div>
+      )}
+    </div>
+  );
 };
+
+export default Navbar;
