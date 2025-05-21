6076911b3c23e08f283076fc568f75a8a5a4b6e7
Adam Dabre <adamdabre23@gmail.com>
2025-05-21 13:52:26 +0800

    [Add] Adding new income form completed

--- frontend/src/pages/Dashboard/Income.jsx
+++ frontend/src/pages/Dashboard/Income.jsx
@@ -1,79 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
-import { useState } from "react";
 import { API_PATHS } from "../../utils/apiPaths";
-import { useEffect } from "react";
 import IncomeOverview from "../../components/Income/IncomeOverview";
 import axiosInstance from "../../utils/axiosInstance";
 import Modal from "../../components/Modal";
+import AddIncomeForm from "../../components/Income/AddIncomeForm";
 
 const Income = () => {
-  const [incomeData, setIncomeData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [openDeleteAlert, setOpenDeleteAlert] = useState({
-    show: false,
-    data: null,
-  });
-  const [openAddIncomeModel, setOpenAddIncomeModel] = useState(false);
+    const [incomeData, setIncomeData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [openDeleteAlert, setOpenDeleteAlert] = useState({
+        show: false,
+        data: null,
+    });
+    const [openAddIncomeModel, setOpenAddIncomeModel] = useState(false);
 
-  // Get All Income Details
-  const fetchIncomeDetails = async () => {
-    if (loading) return;
+    // Get All Income Details
+    const fetchIncomeDetails = async () => {
+        if (loading) return;
 
-    setLoading(true);
+        setLoading(true);
 
-    try {
-      const response = await axiosInstance.get(
-        `${API_PATHS.INCOME.GET_ALL_INCOME}`
-      );
+        try {
+            const response = await axiosInstance.get(
+                `${API_PATHS.INCOME.GET_ALL_INCOME}`
+            );
 
-      if (response.data) {
-        setIncomeData(response.data);
-      }
-    } catch (error) {
-      console.log("Something went wrong. Please try again.", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+            if (response.data) {
+                setIncomeData(response.data);
+            }
+        } catch (error) {
+            console.log("Something went wrong. Please try again.", error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  // Handle Add Income
-  const handleAddIncome = async (income) => {};
+    // Handle Add Income
+    const handleAddIncome = async (income) => {};
 
-  // Delete Income
-  const deleteIncome = async (id) => {};
+    // Delete Income
+    const deleteIncome = async (id) => {};
 
-  // Handle download income details
-  const handleDownloadIncomeDetails = async () => {};
+    // Handle download income details
+    const handleDownloadIncomeDetails = async () => {};
 
-  useEffect(() => {
-    fetchIncomeDetails();
+    useEffect(() => {
+        fetchIncomeDetails();
+        return () => {};
+    }, []);
 
-    return () => {};
-  }, []);
+    return (
+        <DashboardLayout activeMenu="Income">
+            <div className="my-5 mx-auto">
+                <div className="grid grid-cols-1 gap-6">
+                    <div className="">
+                        <IncomeOverview
+                            transactions={incomeData}
+                            onAddIncome={() => setOpenAddIncomeModel(true)}
+                        />
+                    </div>
+                </div>
 
-  return (
-    <DashboardLayout activeMenu="Income">
-      <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 gap-6">
-          <div className="">
-            <IncomeOverview
-              transactions={incomeData}
-              onAddIncome={() => setOpenAddIncomeModal(true)}
-            />
-          </div>
-        </div>
-
-        <Modal
-          isOpen={openAddIncomeModel}
-          onClose={() => setOpenAddIncomeModel(false)}
-          title="Add Income"
-        >
-          <AddIncomeForm onAddIncome={handleAddIncome} />
-        </Modal>
-      </div>
-    </DashboardLayout>
-  );
+                <Modal
+                    isOpen={openAddIncomeModel}
+                    onClose={() => setOpenAddIncomeModel(false)}
+                    title="Add Income"
+                >
+                    <AddIncomeForm onAddIncome={handleAddIncome} />
+                </Modal>
+            </div>
+        </DashboardLayout>
+    );
 };
 
 export default Income;
