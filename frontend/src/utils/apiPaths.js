d9f6bfc46dd68d1ae30eaab02bbee726ce0205c6
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-05-23 12:00:31 +0800

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_BACKEND_URL
+    : "http://localhost:5000";
 
 // utils/apiPaths.js
 export const API_PATHS = {
